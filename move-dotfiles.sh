a5f32f34df4118ecebc3855bc1467d3ce8d892f1
dunleavyd14 <dunleavyd14@gmail.com>
2019-11-24 16:56:10 -0500

    Added .bashrc, changed file structure

--- move-dotfiles.sh
+++ move-dotfiles.sh
@@ -3,5 +3,12 @@
 #script to install dotfiles to their correct locations
 
 #vim
-cp .vimrc ~/.vimrc
+echo "Moving vim dotfiles..."
+cp vim/.vimrc ~/.vimrc
+
+
+#bash
+#TODO: need to setup other bash conf files
+echo "Moving bash dotfiles..."
+cp bash/.bashrc ~/.bashrc
  
